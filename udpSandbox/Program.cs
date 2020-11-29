9739cfd428b8baabb2f8ebf12590823f52709f69
Daniil <dan.unarov@gmail.com>
2020-11-29 18:51:52 +0300

    init

---
+++ udpSandbox/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udpSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            client.Connect("localhost", 8001);
+            string message = "Hello world!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            int numberOfSentBytes = client.Send(data, data.Length);
+            client.Close();
+            Console.WriteLine(numberOfSentBytes);
+        }
+    }
+}
